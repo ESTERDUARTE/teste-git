ba508b9372431b2e6d2634faa02e0b686802e8bb
Lab103-16 <Lab103-16@Lab103-16-PC>
2015-03-24 10:33:27 -0300

    instalação dos arquivos base do sistema

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <hearder>
+        <div class="logo"></div>
+        <div class="menu">
+            <ul class="carrinho">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </hearder>
+</body>
+</html>
+    
+
